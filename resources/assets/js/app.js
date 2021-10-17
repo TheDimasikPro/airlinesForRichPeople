ed928a29c390878bde9759b6536f0fbce92e7e80
Dimasik Pro <dima.site1806@gmail.com>
2021-10-17 23:56:51 +0500

    начата верстка главной страницы сайта

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,1 +1,125 @@
-require('./bootstrap');
+$(document).ready(function(){
+    // переменные
+    const dropbtn_from_flights = $('#dropbtn_from_flights');
+    const dropbtn_to_flights = $('#dropbtn_to_flights');
+    const input_search_from_flights = $('#id_i_s_f_f');
+    const input_search_to_flights = $('#id_i_s_f_t');
+    const flights_list_item = $('.flights_list_item');
+
+    // функции
+    function showdropDownListCitiesSearchFlights(button,dropdown_info){
+        if(!$(button).hasClass('rotate_180')){
+            $(button).addClass('rotate_180');
+        }
+        else{
+            $(button).removeClass('rotate_180');
+        }
+        if(!$(dropdown_info).hasClass('show_drop_content')){
+            $(dropdown_info).addClass('show_drop_content');
+        }
+        else{
+            $(dropdown_info).removeClass('show_drop_content');
+        }
+    }
+
+    function searchInFlightsList(id_input, filter, elements) {
+        $(elements).each(function(i,elem) { // пробегаем все элементы списка
+            let valueLi = elem.innerHTML; // передаем значение инпута в перменную
+            if (valueLi.toUpperCase().indexOf(filter) > -1) { // если найденный индекс элемента > 1
+                elements[i].style.display = "";
+                $(elements[i]).addClass('select_elem_airport');
+                $('#'+id_input+'.dropdown_content__item').removeClass('show_drop_content');
+                console.log($(elements[i]));
+            }
+            else {
+                elements[i].style.display = "none";
+                $('#'+id_input+'.dropdown_content__item').addClass('show_drop_content');
+            }
+        });
+    }
+
+    function moveCaretToEnd(el) {
+        if (typeof el.selectionStart == "number") {
+            el.selectionStart = el.selectionEnd = el.value.length;
+        } else if (typeof el.createTextRange != "undefined") {
+            var range = el.createTextRange();
+            range.collapse(false);
+            range.select();
+        }
+    }
+
+
+    flights_list_item.click(function () {
+        if ($(this).hasClass('check_in')) {
+            $('.check_in_block').removeClass('non_view');
+            $('.form_search_block_inputs').addClass('non_view');
+        }
+        else if ($(this).hasClass('buy_ticket')) {
+            $('.check_in_block').addClass('non_view');
+            $('.form_search_block_inputs').removeClass('non_view');
+        }
+        
+        $('.flights_list_item').each(function (i,elem) {
+            $(elem).removeClass('active_flights_list_item');
+        });
+        $(this).addClass('active_flights_list_item');
+        $('.dropdown_content').each(function(i, elem) {
+            $(elem).removeClass('show_drop_content');
+        });
+        
+    });
+
+    dropbtn_from_flights.click(function(){
+        $('.drop_to_flights').removeClass('show_drop_content');
+        $(dropbtn_to_flights).removeClass('rotate_180');
+        var drop_from_flights_elem = $('.drop_from_flights');
+        input_search_from_flights.focus();
+        moveCaretToEnd(input_search_from_flights);
+        showdropDownListCitiesSearchFlights(this,drop_from_flights_elem);
+    });
+    dropbtn_to_flights.click(function(){
+        $('.drop_from_flights').removeClass('show_drop_content');
+        $(dropbtn_from_flights).removeClass('rotate_180');
+        input_search_to_flights.focus();
+        moveCaretToEnd(input_search_to_flights);
+        var dropbtn_to_flights_elem = $('.drop_to_flights');
+        showdropDownListCitiesSearchFlights(this,dropbtn_to_flights_elem);
+    });
+    input_search_from_flights.click(function(){
+        $('.drop_to_flights').removeClass('show_drop_content');
+        $(dropbtn_to_flights).removeClass('rotate_180');
+        var drop_from_flights_elem = $('.drop_from_flights');
+        if(!$('.drop_from_flights').hasClass('show_drop_content')){
+            showdropDownListCitiesSearchFlights(dropbtn_from_flights,drop_from_flights_elem);
+        }
+    });
+    input_search_to_flights.click(function(){
+        $('.drop_from_flights').removeClass('show_drop_content');
+        $(dropbtn_from_flights).removeClass('rotate_180');
+        var drop_to_flights_elem = $('.drop_to_flights');
+        if(!$('.drop_to_flights').hasClass('show_drop_content')){
+            showdropDownListCitiesSearchFlights(dropbtn_to_flights,drop_to_flights_elem);
+        }
+    });
+
+
+    $('.drop_from_flights .dropdown_content__item').click(function (e) {
+        $('#id_i_s_f_f').val(e.target.innerText);
+        $('.drop_from_flights').removeClass('show-drop-content');
+        e.target.classList.add("select_elem_airport");
+        // $('.id_country').text("");
+    });
+
+
+
+    input_search_from_flights.keyup(function () {
+        var id_input = $(this).attr("id");
+        var filter = $(this).val().toUpperCase(); // приводим все к верхнему регистру
+        var elements = $('.drop_from_flights .dropdown_content__item'); // все элементы с классом dropdown-content--item
+        searchInFlightsList(id_input,filter,elements);
+        if($(this).val() === ""){
+            $(elements).removeClass('select_elem_airport');
+            console.log('cccc');
+        }
+    });
+});
---
+++ resources/views/inc/header.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/assets/css/style.min.css">
+    <link rel="prefetch" as="font" type="font/ttf" href="/assets/fonts/Roboto-Regular.ttf" crossorigin>
+    <link rel="prefetch" as="font" type="font/ttf" href="/assets/fonts/Roboto-Bold.ttf" crossorigin>
+    <script src="https://kit.fontawesome.com/0b1222bcc2.js" crossorigin="anonymous"></script>
+    <title>AirlinesForRichPeople</title>
+</head>
+<body>
+    
+<header id="header">
+    <div class="container">
+        <!-- мобильное меню -->
+        <div class="m_menu"></div>
+        <!-- /.m_menu -->
+        <!-- десктопное меню -->
+        <div class="d_menu">
+            <div class="logo_image">
+                <a href="" class="link_header">
+                    <img src="/assets/images/logo.png" alt="" class="logo">
+                </a> 
+                <!-- /.link_header -->
+            </div>
+            <!-- /.logo_image -->
+            <div class="search">
+                <form action="" class="form form_search">
+                    <span class="span_big_input">
+                        <input type="text" class="big_input search_input" placeholder="Поиск по сайту" autocomplete="off">
+                        <div class="bottom_slider_big_input"></div>
+                        <!-- /.bottom_slider_big_input -->
+                    </span>
+                    <!-- /.span_big_input -->
+                    <button>
+                        <i class="fas fa-search"></i>
+                    </button>
+                </form>
+                <!-- /.form form_search -->
+            </div>
+            <!-- /.search -->
+            <ul class="nav_menu">
+                <li class="geo_posistion_people">
+                    <i class="fas fa-globe"></i>
+                    <p class="geo_info">
+                        RU <span>(RUB, р)</span>
+                    </p>
+                    <!-- /.geo_info -->
+                </li>
+                <!-- /.geo_posistion_people -->
+                <li class="my_cabinet">
+                    <a href="" class="link_header">
+                        <i class="fas fa-user"></i>
+                    </a>
+                    <!-- /.link_header -->
+                </li>
+                <!-- /.my_cabinet -->
+            </ul>
+            <!-- /.nav_menu -->
+        </div>
+        <!-- /.d_menu -->
+    </div>
+    <!-- /.container -->
+</header>
