29939cf154dab219c628654c62b7953aca296df2
Dimasik Pro <dima.site1806@gmail.com>
2021-10-23 15:50:02 +0500

    верстка регистрации почти готова
    доделать анимацию

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,10 +4,14 @@ $(document).ready(function(){
     const dropbtn_to_flights = $('#dropbtn_to_flights');
     const input_search_from_flights = $('#id_i_s_f_f');
     const input_search_to_flights = $('#id_i_s_f_t');
+    const input_country_of_issue = $('#input_country_of_issue');
     const flights_list_item = $('.flights_list_item');
     const input_count_pass_block = $('.input_count_pass_block');
     const input_count_pass = $('#id_i_c_pass');
     const dropbtn_prefix_phone = $('#dropbtn_prefix_phone');
+    const dropbtn_gender_code = $('#dropbtn_gender_code');
+    const dropbtn_type_document = $('#dropbtn_type_document');
+    const dropbtn_country_of_issue = $('#dropbtn_country_of_issue');
 
     
     
@@ -33,6 +37,7 @@ $(document).ready(function(){
             }
         });
     }
+    
 
     function moveCaretToEnd(el) {
         if (typeof el.selectionStart == "number") {
@@ -305,6 +310,55 @@ $(document).ready(function(){
                     $(dropbtn_prefix_phone).removeClass('rotate_180');
                 }
             }
+            if (!target.closest('#dropbtn_gender_code')) {
+                var dropbtn_gender_code = $('#dropbtn_gender_code');
+                if (target.closest('.form_auth_input[id="gender_code"]')) {
+                    if (!$('.gender_code_list').hasClass('show_drop_content')) {
+                        showdropDownListCitiesSearchFlights(dropbtn_gender_code,$('.gender_code_list'));
+                    }
+                    else{
+                        $('.gender_code_list').removeClass('show_drop_content');
+                        (dropbtn_gender_code).removeClass('rotate_180');
+                    }
+                }
+                else{
+                    $('.gender_code_list').removeClass('show_drop_content');
+                    $(dropbtn_gender_code).removeClass('rotate_180');
+                }
+            }
+            if (!target.closest('#dropbtn_type_document')) {
+                var dropbtn_type_document = $('#dropbtn_type_document');
+                if (target.closest('.form_auth_input[id="type_document"]')) {
+                    if (!$('.type_document_list').hasClass('show_drop_content')) {
+                        showdropDownListCitiesSearchFlights(dropbtn_type_document,$('.type_document_list'));
+                    }
+                    else{
+                        $('.type_document_list').removeClass('show_drop_content');
+                        (dropbtn_type_document).removeClass('rotate_180');
+                    }
+                }
+                else{
+                    $('.type_document_list').removeClass('show_drop_content');
+                    $(dropbtn_type_document).removeClass('rotate_180');
+                }
+            }
+            if (!target.closest('#dropbtn_country_of_issue')) {
+                var dropbtn_country_of_issue = $('#dropbtn_country_of_issue');
+                if (target.closest('.form_auth_input[id="input_country_of_issue"]')) {
+                    $('.form_auth_input[id="input_country_of_issue"]').autocomplete = 'off';;
+                    if (!$('.country_of_issue_list').hasClass('show_drop_content')) {
+                        showdropDownListCitiesSearchFlights(dropbtn_country_of_issue,$('.country_of_issue_list'));
+                    }
+                    else{
+                        $('.country_of_issue_list').removeClass('show_drop_content');
+                        (dropbtn_country_of_issue).removeClass('rotate_180');
+                    }
+                }
+                else{
+                    $('.country_of_issue_list').removeClass('show_drop_content');
+                    $(dropbtn_country_of_issue).removeClass('rotate_180');
+                }
+            }
         }
     });
     input_count_pass_block.click(function () {
@@ -492,18 +546,98 @@ $(document).ready(function(){
     $('.prefix_phone_list__item').click(function () {
         var prefix_phone__text = $(this).text();
         $('#prefix_phone').val(prefix_phone__text);
-        $(this).addClass('select_prefix_phone');
+        $('.prefix_phone_list__item').removeClass('select_list__item');
+        $(this).addClass('select_list__item');
     });
 
+    dropbtn_gender_code.click(function (e) {
+        e.preventDefault();
+        if (!$('.gender_code_list').hasClass('show_drop_content')) {
+            showdropDownListCitiesSearchFlights(this,$('.gender_code_list'));
+        }
+        else{
+            $('.gender_code_list').removeClass('show_drop_content');
+            $(this).removeClass('rotate_180');
+        }
+    });
+    $('.gender_code_list__item').click(function () {
+        var gender_code__text = $(this).text();
+        $('#gender_code').val(gender_code__text);
+        $('.gender_code_list__item').removeClass('select_list__item');
+        $(this).addClass('select_list__item');
+    });
 
+    dropbtn_type_document.click(function (e) {
+        e.preventDefault();
+        if (!$('.type_document_list').hasClass('show_drop_content')) {
+            showdropDownListCitiesSearchFlights(this,$('.type_document_list'));
+        }
+        else{
+            $('.type_document_list').removeClass('show_drop_content');
+            $(this).removeClass('rotate_180');
+        }
+    });
+    $('.type_document_list__item').click(function () {
+        var type_document__text = $(this).text();
+        $('#type_document').val(type_document__text);
+        $('.type_document_list__item').removeClass('select_list__item');
+        $(this).addClass('select_list__item');
+    });
+    $("#series_document_number").mask("99 99 999999",
+    {
+        completed:function () {
+            console.log("харош");
+        }
+    });
     $("#phone").mask("(999)999-999-9",
     {
         completed:function () {
             console.log("харош");
         }
     });
-
-
+    dropbtn_country_of_issue.click(function (e) {
+        e.preventDefault();
+        if (!$('.country_of_issue_list').hasClass('show_drop_content')) {
+            showdropDownListCitiesSearchFlights(this,$('.country_of_issue_list'));
+        }
+        else{
+            $('.country_of_issue_list').removeClass('show_drop_content');
+            $(this).removeClass('rotate_180');
+        }
+    });
+    $('.country_of_issue_list__item').click(function () {
+        var type_country__text = $(this).text();
+        $('#input_country_of_issue').val(type_country__text);
+        $('.country_of_issue_list__item').removeClass('select_list__item');
+        $(this).addClass('select_list__item');
+    });
+    function searchСountryOfIssue(id_input, filter, elements) {
+        $(elements).each(function(i,elem) { // пробегаем все элементы списка
+            let valueLi = elem.innerHTML; // передаем значение инпута в перменную
+            
+            if (valueLi.toUpperCase().indexOf(filter) > -1) { // если найденный индекс элемента > 1
+                elements[i].style.display = "";
+                $(elements[i]).addClass('select_list__item');
+                $('.country_of_issue_list').addClass('show_drop_content');
+                $('#'+id_input+'.country_of_issue_list__item').removeClass('show_drop_content');
+            }
+            else {
+                elements[i].style.display = "none";
+                // $(elements[i]).removeAttr('style');
+                $(elements[i]).removeClass('select_list__item');
+                $('#'+id_input+'.country_of_issue_list__item').addClass('show_drop_content');
+            }
+        });
+    }
+    input_country_of_issue.keyup(function () {
+        var id_input = $(this).attr("id");
+        var filter = $(this).val().toUpperCase(); // приводим все к верхнему регистру
+        var elements = $('.country_of_issue_list .country_of_issue_list__item'); // все элементы с классом dropdown-content--item
+        searchСountryOfIssue(id_input,filter,elements);
+        if($(this).val() === ""){
+            $(elements).removeClass('select_list__item');
+        }
+    });
     $('#complete_contact_data').click(function (e) {
         e.preventDefault();
         $('.form_auth_contact_data').addClass('form_auth_card__anim');
@@ -514,25 +648,43 @@ $(document).ready(function(){
             $('#auth_block_btn').removeAttr('style');
         }, 50);
         setTimeout(() => {
-            
-            $('.form_auth_contact_data').animate({
-                marginLeft: "-1500px",
-                opacity: 0
-            },2000);
-            
             setTimeout(() => {
-                // $('.form_auth_contact_data').css('padding','0');
-                // $('.form_auth_contact_data').css('margin-left','0');
-                
-                $('.form_auth_contact_data').css('display','none');
-            }, 1150);
-            // $('.form_auth_contact_data').css('margin-left','-1500px');
-            // $('.form_auth_contact_data').css('display','none');
-            // $('.form_auth_contact_data').removeClass('form_auth_card__anim');
-            // $('#auth_block_btn').removeClass('form_auth_block__anim');
-            // $('#auth_block_btn').removeAttr('style');
-            // $('.form_auth_contact_data').removeAttr('style');
-        }, 800);
+                $('.form_auth_profile_data').css('display',"block");
+            }, 650);
+            $('.form_auth_profile_data').animate({
+                marginLeft: "0px",
+                opacity: "1",
+            },1200);
+            $('.form_auth_contact_data').animate({
+                marginLeft: "-1500px"
+            },700);
+            setTimeout(() => {
+                $('.form_auth_profile_data').removeClass('form_auth_non_view');
+                $('.stages_list__item').removeClass('stages_list__item_active');
+                $('.stages_list__item:nth-child(2)').addClass('stages_list__item_active');
+            }, 1380);
+            setTimeout(() => {
+                // $('.form_auth_contact_data').css('display','none');
+                $('.form_auth_contact_data').addClass('form_auth_card__complete_anim');
+            }, 700);
+        }, 1000);
         
-    })
+    });
+
+    $('#show_passwod').click(function (e) {
+        e.preventDefault();
+        if (!$(this).hasClass('non_view')) {
+            $(this).addClass('non_view');
+            $('#input_password').attr('type','text');
+            $('#hide_passwod').removeClass('non_view');
+        }
+    });
+    $('#hide_passwod').click(function (e) {
+        e.preventDefault();
+        if (!$(this).hasClass('non_view')) {
+            $(this).addClass('non_view');
+            $('#input_password').attr('type','password');
+            $('#show_passwod').removeClass('non_view');
+        }
+    });
 });
--- resources/views/Auth/reg.blade.php
+++ resources/views/Auth/reg.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app_layout')
-@section('title_page','Авторизация')
+@section('title_page','Регистрация')
 @section('styles_link')
     <link rel="stylesheet" href="/assets/css/auth.min.css">
 @endsection
@@ -24,7 +24,7 @@
                 </div>
                 <!-- /.form_auth__row -->
                 <div class="form_auth__row">
-                    <form action="" class="form_auth_contact_data">
+                    <form action="" class="form_auth_contact_data form_auth_non_view" autocomplete="off">
                         <div class="form_auth_block">
                             <label for="email">E-mail</label>
                             <input type="email" autocomplete class="super_big_input form_auth_input" id="email" name="email" placeholder="Введите свою почту">
@@ -36,7 +36,7 @@
                             <div class="form_auth_block">
                                 <label for="prefix_phone">Prefix phone</label>
                                 <input type="tel" autocomplete class="min_input form_auth_input" readonly id="prefix_phone" name="phone" value="+7">
-                                <button class="dropbtn" type="button" id="dropbtn_prefix_phone">
+                                <button class="dropbtn" type="button" id="dropbtn_prefix_phone" aria-label="dropbtn_prefix_phone">
                                     <i class="fas fa-arrow-down" id=""></i>
                                 </button>
                                 
@@ -92,7 +92,7 @@
                             <!-- /.form_auth_block -->
                             <div class="form_auth_block">
                                 <label for="phone">Phone</label>
-                                <input type="tel" autocomplete class="big_input form_auth_input" id="phone" name="phone" placeholder="(***) ***-**-**">
+                                <input type="tel" autocomplete class="big_input form_auth_input" id="phone" name="phone" placeholder="Введите номер своего телефона">
                                 <div class="form_auth__slider_input"></div>
                                 <!-- /.form_auth__slider_input -->
                             </div>
@@ -101,15 +101,15 @@
                         <!-- /.form_auth__row -->
                         
                         <div class="form_auth_block">
-                            <button class="btn btn_style_1 upper" type="button" id="complete_contact_data">Продолжить</button> 
+                            <button class="btn btn_style_1 upper" type="button" id="complete_contact_data" aria-label="complete_contact_data">Продолжить</button> 
                             <!-- /.btn btn_style_1 upper-->
                         </div>
                     </form>
                     <!-- /.form_auth_contact_data -->
-                    <form action="" class="form_auth_profile_data">
+                    <form action="" class="form_auth_profile_data form_auth_non_view" autocomplete="off">
                         <div class="form_auth_block">
                             <label for="full_name">Full name</label>
-                            <input type="text" autocomplete class="super_big_input form_auth_input" id="full_name" name="full_name" placeholder="Введите полное имя">
+                            <input type="text" autocomplete="new" class="super_big_input form_auth_input" id="full_name" name="full_name" placeholder="Введите полное имя">
                             <div class="form_auth__slider_input"></div>
                             <!-- /.form_auth__slider_input -->
                         </div>
@@ -117,61 +117,79 @@
                         <div class="form_auth__row df_jcspb_aic">
                             <div class="form_auth_block">
                                 <label for="date_birthday">Date birthday</label>
-                                <input type="date" autocomplete class="sm_input_2 form_auth_input" id="date_birthday" name="date_birthday" placeholder="Дата рождения">
+                                <input type="date" autocomplete="new" class="sm_input_2 form_auth_input" id="date_birthday" name="date_birthday" placeholder="Дата рождения">
                                 <div class="form_auth__slider_input"></div>
                             </div>
                             <!-- /.form_auth_block -->
                             <div class="form_auth_block">
                                 <label for="gender_code">Gender</label>
-                                <input type="text" autocomplete class="sm_input_2 form_auth_input" readonly id="gender_code" name="gender_code" placeholder="Пол">
-                                <button class="dropbtn" type="button" id="dropbtn_gender_code">
+                                <input type="text" autocomplete="new" class="sm_input_2 form_auth_input" readonly id="gender_code" name="gender_code" placeholder="Пол">
+                                <button class="dropbtn" type="button" id="dropbtn_gender_code" aria-label="dropbtn_gender_code">
                                     <i class="fas fa-arrow-down" id=""></i>
                                 </button>
                                 
                                 <div class="form_auth__slider_input"></div>
                                 <!-- /.form_auth__slider_input -->
                                 <ul class="gender_code_list">
-                                    <li class="gender_code_list__item">Мужской</li>
-                                    <li class="gender_code_list__item">Женский</li>
+                                    <li class="gender_code_list__item">Мужской / Male</li>
+                                    <li class="gender_code_list__item">Женский / Woman</li>
                                 </ul>
                             </div>
                         </div>
                         <div class="form_auth_block">
                             <label for="city_name">City</label>
-                            <input type="text" autocomplete class="super_big_input form_auth_input" id="city_name" name="city_name" placeholder="Введите название своего города">
+                            <input type="text" autocomplete="new" class="super_big_input form_auth_input" id="city_name" name="city_name" placeholder="Введите название своего города">
                             <div class="form_auth__slider_input"></div>
                             <!-- /.form_auth__slider_input -->
                         </div>
                         <div class="form_auth__row df_jcspb_aic">
                             <div class="form_auth_block">
-                                <label for="date_birthday">Date birthday</label>
-                                <input type="date" autocomplete class="sm_input_2 form_auth_input" id="date_birthday" name="date_birthday" placeholder="Дата рождения">
-                                <div class="form_auth__slider_input"></div>
-                            </div>
-                            <!-- /.form_auth_block -->
-                            <div class="form_auth_block">
-                                <label for="gender_code">Gender</label>
-                                <input type="text" autocomplete class="sm_input_2 form_auth_input" readonly id="gender_code" name="gender_code" placeholder="Пол">
-                                <button class="dropbtn" type="button" id="dropbtn_gender_code">
+                                <label for="type_document">Type document</label>
+                                <input type="text" autocomplete="new" class="sm_input_2 form_auth_input" readonly id="type_document" name="type_document" placeholder="Тип документа">
+                                <button class="dropbtn" type="button" id="dropbtn_type_document" aria-label="dropbtn_type_document">
                                     <i class="fas fa-arrow-down" id=""></i>
                                 </button>
                                 
                                 <div class="form_auth__slider_input"></div>
                                 <!-- /.form_auth__slider_input -->
-                                <ul class="gender_code_list">
-                                    <li class="gender_code_list__item">Мужской</li>
-                                    <li class="gender_code_list__item">Женский</li>
+                                <ul class="type_document_list">
+                                    <li class="type_document_list__item">Мужской</li>
+                                    <li class="type_document_list__item">Женский</li>
+                                    <li class="type_document_list__item">Мужской</li>
+                                    <li class="type_document_list__item">Женский</li>
+                                    <li class="type_document_list__item">Мужской</li>
+                                    <li class="type_document_list__item">Женский</li>
+                                    <li class="type_document_list__item">Мужской</li>
+                                    <li class="type_document_list__item">Женский</li>
+                                </ul>
+                            </div>
+                            <div class="form_auth_block">
+                                <label for="series_document_number">Series and document number</label>
+                                <input type="text" autocomplete="new" class="sm_input_2 form_auth_input" id="series_document_number" name="series_document_number" placeholder="Серия и номер документа">
+                                <div class="form_auth__slider_input"></div>
+                                <!-- /.form_auth__slider_input -->
+                            </div>
+                            <!-- /.form_auth_block -->
+                        </div>
+                        <div class="form_auth__row">
+                            <div class="form_auth_block">
+                                <label for="input_country_of_issue">Country of issue</label>
+                                <input type="text" autocomplete="new" class="super_big_input form_auth_input" id="input_country_of_issue" name="country_of_issue" placeholder="Страна выдачи">
+                                <button class="dropbtn" type="button" id="dropbtn_country_of_issue" aria-label="dropbtn_country_of_issue">
+                                    <i class="fas fa-arrow-down" id=""></i>
+                                </button>
+                                
+                                <div class="form_auth__slider_input"></div>
+                                <!-- /.form_auth__slider_input -->
+                                <ul class="country_of_issue_list">
+                                    <li class="country_of_issue_list__item">Мужской</li>
+                                    <li class="country_of_issue_list__item">Женский</li>
+
                                 </ul>
                             </div>
                         </div>
                         <div class="form_auth_block">
-                            <label for="city_name">City</label>
-                            <input type="text" autocomplete class="super_big_input form_auth_input" id="city_name" name="city_name" placeholder="Введите название своего города">
-                            <div class="form_auth__slider_input"></div>
-                            <!-- /.form_auth__slider_input -->
-                        </div>
-                        <div class="form_auth_block">
-                            <button class="btn btn_style_1 upper" type="button" id="complete_contact_data">Продолжить</button> 
+                            <button class="btn btn_style_1 upper" type="button" id="complete_profile_data" aria-label="complete_profile_data">Продолжить</button> 
                             <!-- /.btn btn_style_1 upper-->
                         </div>
                         <!-- /.form_auth_block -->
@@ -185,6 +203,25 @@
                         <!-- /.form_auth_block --> --}}
                     </form>
                     <!-- /.form_auth_profile_data -->
+                    <div class="form_auth_password_data">
+                        <div class="form_auth_block">
+                            <label for="password">Password</label>
+                            <input type="password" autocomplete="new" class="super_big_input form_auth_input" id="input_password" name="password" placeholder="Введите свой пароль">
+                            <button class="dropbtn" type="button" id="show_passwod" aria-label="btn_show_passwod">
+                                <i class="far fa-eye-slash"></i>
+                            </button>
+                            <button class="dropbtn non_view" type="button" id="hide_passwod" aria-label="btn_hide_passwod">
+                                <i class="far fa-eye"></i>
+                            </button>
+                            <div class="form_auth__slider_input"></div>
+                            <!-- /.form_auth__slider_input -->
+                        </div>
+                        <div class="form_auth_block">
+                            <button class="btn btn_style_1 upper" type="button" id="complete_password_data" aria-label="complete_password_data">Продолжить</button> 
+                            <!-- /.btn btn_style_1 upper-->
+                        </div>
+                    </div>
+                    <!-- /.form_auth_password_data form_auth_non_view -->
                 </div>
                 <!-- /.form_auth__row -->
             </div>
